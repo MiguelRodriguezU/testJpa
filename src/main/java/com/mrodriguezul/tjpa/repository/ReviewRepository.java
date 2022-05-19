69c746711148695ba974d448c24c8bc227ad1992
Miguel Rod <miguel.rodriguez.u@gmail.com>
2022-05-19 07:40:45 -0500

    delete

--- src/main/java/com/mrodriguezul/tjpa/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mrodriguezul.tjpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mrodriguezul.tjpa.model.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Integer>{
-
-}
